58d60701ab2427f82a34eb634a422eb6378efebd
johnallec <clegnn95p30c352m@studenti.unical.it>
2021-12-07 15:02:55 +0100

    first commit

---
+++ src/main/java/com/battlefoo/BattleFooApplication.java
@@ -0,0 +1,13 @@
+package com.battlefoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleFooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleFooApplication.class, args);
+	}
+
+}
