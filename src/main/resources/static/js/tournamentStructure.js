526f89f71634dee05b31a1c3c007983d0ffd5ff8
Nambon03 <deciccomarco@hotmail.it>
2022-01-04 20:01:39 +0100

    riordinamento file progetto web

---
+++ src/main/resources/static/js/tournamentStructure.js
@@ -0,0 +1,43 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active')
+    })
+    tabs.forEach(tab => {
+      tab.classList.remove('active')
+    })
+    tab.classList.add('active')
+    target.classList.add('active')
+    if(!target.className.includes("overview")) {
+        document.getElementsByClassName("overview_box")[0].style.display="none";
+    }
+    else {
+        document.getElementsByClassName("overview_box")[0].style.display="block";
+
+    }
+  })
+});
+
+function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+  console.log(evt.currentTarget.className);
+}
