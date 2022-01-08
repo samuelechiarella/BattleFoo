1be05ff4b6c2052f1a5351003406d5423bfa420b
johnallec <clegnn95p30c352m@studenti.unical.it>
2022-01-08 06:29:00 +0100

    testing postgres: it works

---
+++ src/main/java/com/battlefoo/Database.java
@@ -0,0 +1,30 @@
+package com.battlefoo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Database {
+	Connection conn;
+	public Database() {
+		try {
+			conn = DriverManager.getConnection("jdbc:postgresql://postgres:5432/postgres", "postgres", "76199065");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void insertGame(String game, String genre) {
+		try {
+			PreparedStatement st = conn.prepareStatement("insert into game values(?,?);");
+			st.setString(1, game);
+			st.setString(2, genre);
+			System.out.println(st.executeUpdate()); 
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
--- src/main/java/com/battlefoo/controller/HomePage.java
+++ src/main/java/com/battlefoo/controller/HomePage.java
@@ -3,10 +3,13 @@ package com.battlefoo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.battlefoo.Database;
+
 @Controller
 public class HomePage {
 	@GetMapping("/")
 	public String getHomePage() {
+		Database db = new Database();
 		return "index";
 	}
 }
