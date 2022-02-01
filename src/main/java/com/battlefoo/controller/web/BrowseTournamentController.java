b9b0298f697d188f5ebffba56bad89160d7b4aa6
johnallec <clegnn95p30c352m@studenti.unical.it>
2022-02-01 23:00:37 +0100

    team images bugs fixed

--- src/main/java/com/battlefoo/controller/web/BrowseTournamentController.java
+++ src/main/java/com/battlefoo/controller/web/BrowseTournamentController.java
@@ -1,16 +1,35 @@
 package com.battlefoo.controller.web;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.battlefoo.ServerPaths;
+import com.battlefoo.model.entitiesObjects.Tournament;
 import com.battlefoo.persistence.dbManagement.Database;
 
 @Controller
 public class BrowseTournamentController {
 	@GetMapping("/BrowseTournament")
 	public String getBrowseTournament(HttpServletRequest req) {
-		req.getSession(true).setAttribute("allTournaments", Database.getInstance().getAllTournaments());
+		List<Tournament> tournaments = Database.getInstance().getAllTournaments();
+		for(Tournament t : tournaments) {
+			if(t.getLogo().compareTo(ServerPaths.DEFAULT_ORGANIZATION_BANNER) != 0) {
+				try {
+					BufferedReader br =  new BufferedReader(new FileReader(t.getLogo()));
+					t.setLogo(br.readLine());
+					br.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		req.getSession(true).setAttribute("allTournaments", tournaments);
 		return "BrowseTournament";
 	}
 }
