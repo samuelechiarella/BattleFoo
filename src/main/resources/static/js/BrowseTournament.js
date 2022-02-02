16dd450f86804c3149c28e5fc23ba8ef74853d0f
Samuele <chiare.samu@gmail.com>
2022-02-02 19:18:20 +0100

    update browse tournament

---
+++ src/main/resources/static/js/BrowseTournament.js
@@ -0,0 +1,32 @@
+function applyFilterSearchTournament(){
+	if($("#tournamentName").val() == null){return;}
+	let tbody = document.getElementById("filterTournamentTableBody");
+	while(tbody.hasChildNodes())
+		tbody.removeChild(tbody.firstChild);
+		
+	$.ajax({
+		type:"POST",
+		url:"/filterTournament",
+		contentType:"application/json",
+		data: JSON.stringify($("#tournamentName").val()),
+		success: function(tournament){
+			if (tournament == null){
+				console.log(" TOURNAMENT LIST IS NULL");
+				return;
+				}
+			for(let i = 0; i < tournament.length; ++i) {
+			insertNewTournament(tournament[i][0],tournament[i][1],tournament[i][2],tournament[i][3]);
+				}
+			},
+			error: function(err){
+			console.log(err);
+			}
+		});
+	}
+	function insertNewTournament(logo, name, data,id){
+	var row= "<tr onclick=\"openTournamentPage('" + Number(id) + "')\"><td><img src='"
+			+ logo + "'></td><td>" + name + "</td><td>" + data + "</td></tr>";
+  	$('#filterTournamentTableBody').append(row);
+} 
+	
+	
--- src/main/webapp/WEB-INF/view/jsp/BrowseTournament.jsp
+++ src/main/webapp/WEB-INF/view/jsp/BrowseTournament.jsp
@@ -9,22 +9,26 @@
 </head>
 <body>
 <jsp:include page="background.jsp"></jsp:include>
+<script src="js/BrowseTournament.js"></script>
 
 <div class="PageTitle">
 	<h1>BROWSE	TOURNAMENTS</h1>
 	
 	<div class="FilterArea">
-		<input type="text" class="FilterInput">
-		<button class="FilterBtn">Search</button>
+		<input id = "tournamentName" type="text" class="FilterInput">
+		<button class="FilterBtn" onclick = "applyFilterSearchTournament()">Search</button>
 	</div>
 	
 	<div class="browse-tournament-table">
 		<table>
+		<thead>
 			<tr>
 		  		<th>Category</th>
 		    	<th>Title</th>
 				<th>Date</th>
 			</tr>
+			</thead>
+			<tbody id="filterTournamentTableBody" >
 			<jstl:forEach items="${allTournaments}" var="tournament">
 				<tr onclick="openTournamentPage('${tournament.tournamentId}')">
 					<td>
@@ -38,6 +42,7 @@
 				  	<td>${tournament.date.dateString}</td>
 				</tr>
 			</jstl:forEach>
+			</tbody>
 		</table>
 	</div>
 </div>
